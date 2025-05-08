52840cddb7db52eec181d410c3c2b6027d02a750
Onl1sky <d603947@gmail.com>
2025-05-08 19:56:02 +0300

    Am realizat

--- script.js
+++ script.js
@@ -1,74 +1,59 @@
+//ПЕРЕМЕННЫЕ
 let heroImg = window.document.querySelector('#hero-img');
-heroImg.onclickc = (event) => {
+heroImg.onclick = (event) => {
     event.preventDefault();
 }
 let imgBlock = window.document.querySelector('#img-block');
-let imgIdle = window.document.querySelector('#hero-idle')
 let canvas = window.document.querySelector('#canvas');
-let fsbtn = window.document.querySelector('#fsBtn')
-let Position = 0;
-let imgBlockPosition = 0;
-
-fsbtn.onclick = () => {
+let fsBtn = window.document.querySelector('#fsBtn');
+fsBtn.onclick = () => {
     if(window.document.fullscreen) {
         window.document.exitFullscreen();
     } else {
         canvas.requestFullscreen();
     }
 }
-
-// Functile
+let Position = 0;
+let imgBlockPosition = 0;
+//ФУНКЦИИ
 const rightHandler = () => {
     heroImg.style.transform = "scale(1,1)";
     Position = Position + 1;
     imgBlockPosition = imgBlockPosition + 1;
-    if (Position > 3) {
+    if (Position > 7) {
         Position = 0;
     }
-    heroImg.style.left = `-${Position*288}px`;
-    imgBlock.style.left = `${imgBlockPosition*20}px`;
+    heroImg.style.left = `-${Position * 288}px`;
+    imgBlock.style.left = `${imgBlockPosition * 20}px`;
 }
 const leftHandler = () => {
     heroImg.style.transform = "scale(-1,1)";
     Position = Position + 1;
     imgBlockPosition = imgBlockPosition - 1;
-    if (Position > 3) {
+    if (Position > 7) {
         Position = 0;
     }
-    heroImg.style.left = `-${Position*288}px`
-    imgBlock.style.left = `${imgBlockPosition*20}px`;
-}
+    heroImg.style.left = `-${Position * 288}px`;
+    imgBlock.style.left = `${imgBlockPosition * 20}px`;
+}	
 
-const standHandler = () => {
-    imgIdle.style.transform = "scale(1,1)";
-    Position = Position + 1;
-    if (Position > 3) {
-        Position = 0;
-    }
-    imgIdle.style.left = `-${Position*288}px`;
-}
-
+// ОБРАБОТЧИКИ СОБЫТИЙ
 let timer = null;
-const lifeCycle = () => {
-    timer = setInterval(()=>{
-    standHandler();
-    }, 150);
-}
 let x = 0;
 let halfWidth = window.screen.width / 2;
 let onTouchStart = (event) => {
     clearInterval(timer);
-    x = (event.type === 'mousedown') ? event.screenX : x + event.touches
+    x = (event.type === 'mousedown') ? event.screenX : event.touches[0].screenX;
     timer = setInterval(() => {
-          (x > halfWidth) ? rightHandler() : leftHandler();
-    },130); 
+        (x > halfWidth) ? rightHandler() : leftHandler();
+    }, 130);
 }
 let onTouchEnd = (event) => {
-    timer = clearInterval(timer);
-    lifeCycle();
+    clearInterval(timer);
 }
-//Indeplinerea functie
+
 window.onmousedown = onTouchStart;
 window.ontouchstart = onTouchStart;
+
 window.onmouseup = onTouchEnd;
-window.ontouchend = onTouchEnd;
+window.ontouchend = onTouchEnd;	
