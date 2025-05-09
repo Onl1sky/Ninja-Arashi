08863f3023840c57e4c7c155e1263e42193687b4
Onl1sky <d603947@gmail.com>
2025-05-09 20:59:09 +0300

    O noua greutate cu animatiile rezolvata

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ fsBtn.onclick = () => {
 }
 let Position = 0;
 let imgBlockPosition = 0;
+let direction = 'right';
 //ФУНКЦИИ
 const rightHandler = () => {
     heroImg.style.transform = "scale(1,1)";
@@ -24,6 +25,7 @@ const rightHandler = () => {
         Position = 0;
     }
     heroImg.style.left = `-${Position * 288}px`;
+    heroImg.style.top = '0px';
     imgBlock.style.left = `${imgBlockPosition * 20}px`;
 }
 const leftHandler = () => {
@@ -31,25 +33,61 @@ const leftHandler = () => {
     Position = Position + 1;
     imgBlockPosition = imgBlockPosition - 1;
     if (Position > 7) {
-        Position = 0;
+        Position = 1;
     }
     heroImg.style.left = `-${Position * 288}px`;
+    heroImg.style.top = '0px';
     imgBlock.style.left = `${imgBlockPosition * 20}px`;
 }	
 
+const standHandler = () => {
+    switch (direction) {
+        case 'right': {
+            heroImg.style.transform = "scale(1,1)";
+            if (Position > 4) {
+                Position = 0;
+            }
+            break;
+        }
+        case 'left': {
+            heroImg.style.transform = "scale(-1,1)";
+            if (Position > 7) {
+                Position = 2;
+            }
+            break;
+        }
+        default: break;
+    }
+    Position = Position + 1;
+    heroImg.style.left = `-${Position * 288}px`;
+    heroImg.style.top = '-300px';
+}
+
 // ОБРАБОТЧИКИ СОБЫТИЙ
 let timer = null;
+const lifeCicle = () => {
+    timer = setInterval(() => { 
+        standHandler();
+    }, 150);
+}
 let x = 0;
 let halfWidth = window.screen.width / 2;
 let onTouchStart = (event) => {
     clearInterval(timer);
     x = (event.type === 'mousedown') ? event.screenX : event.touches[0].screenX;
     timer = setInterval(() => {
-        (x > halfWidth) ? rightHandler() : leftHandler();
+        if (x > halfWidth) {
+            direction = 'right';
+            rightHandler();
+        }  else { 
+            direction = 'left';
+            leftHandler();
+        }
     }, 130);
 }
 let onTouchEnd = (event) => {
     clearInterval(timer);
+    lifeCicle();
 }
 
 window.onmousedown = onTouchStart;
@@ -57,3 +95,8 @@ window.ontouchstart = onTouchStart;
 
 window.onmouseup = onTouchEnd;
 window.ontouchend = onTouchEnd;	
+
+const start = () => {
+    lifeCicle();
+}
+start();
